ce76c422fa9395e8ee6d748881413689ee11962e
Tanmay <tanmaygpro@gmail.com>
2021-06-28 14:44:31 +0530

    redis

--- config/kue.js
+++ config/kue.js
@@ -1,5 +1,12 @@
 const kue = require('kue');
 
-const queue = kue.createQueue();
+const queue = kue.createQueue({
+    redis: {
+        port: 14763,
+        host: 'redis-14763.c273.us-east-1-2.ec2.cloud.redislabs.com:14763',
+        auth: 'Tanmay@9590' // if provided select a non-default redis db
+      }
+    }
+);
 
 module.exports = queue;
